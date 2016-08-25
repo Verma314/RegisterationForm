630732b8185ee3b6cd84d02a90ee1d85807874c0
Verma314 <adityasinghverma314@gmail.com>
2016-08-26 01:29:25 +0530

    Add files via upload

---
+++ Banking/src/MainClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author student
+ */
+public class MainClass {
+    public static void main(String args[]) {
+        GUI x1 = new GUI();
+        x1.setVisible(true);
+    }
+    
+}
